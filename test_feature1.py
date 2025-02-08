29a6476f92d6ec0eb72900f0ddb045b17a3b68a6
Boyan Mirov <boyanmirov@Boyans-MacBook-Air-2.local>
2025-02-08 16:31:20 -0500

    add markers

--- test_feature1.py
+++ test_feature1.py
@@ -1,9 +1,26 @@
+import pytest
+
+
 # A sample test function for pytest
 def add_numbers(a, b):
     return a + b
 
 
+@pytest.mark.run
 def test_add_numbers():
+    # Arrange
+    a = 2
+    b = 3
+
+    # Act
+    result = add_numbers(a, b)
+
+    # Assert
+    assert result == 5
+
+
+@pytest.mark.run2
+def test_add_numbers2():
     # Arrange
     a = 2
     b = 3
